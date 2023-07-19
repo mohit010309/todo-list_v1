dd0635fdaae78e7bb8d7dd28fa47a5dc31357d25
Mohit Akhouri <mohitakhouri83@gmail.com>
2023-07-19 23:59:46 +0530

    add items and remove items functionality

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express=require('express');
 const bodyParser=require('body-parser');
+const mongoose=require('mongoose');
 const app=express();
 const date=require(__dirname+'/module1.js');
 // console.log(date());
@@ -8,8 +9,33 @@ app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
-let item=[];
-let work=[];
+// let item=[];
+// let work=[];
+mongoose.connect("mongodb://127.0.0.1:27017/todolistDB");
+const itemsSchema = new mongoose.Schema({
+    name:String
+});
+
+const ItemModel = mongoose.model("item",itemsSchema);
+
+
+// creating default documents ( displayed each day )
+const defDoc1 = new ItemModel({
+    name:"This is default item 1"
+});
+const defDoc2 = new ItemModel({
+    name:"This is default item 2"
+});
+const defDoc3 = new ItemModel({
+    name:"This is default item 3"
+});
+
+const docsArray = [defDoc1,defDoc2,defDoc3];
+
+// adding documents in mongoDB database
+// ItemModel.insertMany(docsArray);
+
+
 
 app.listen(3000,function(){
     console.log("Server is running on port 3000");
@@ -20,29 +46,60 @@ app.get("/",function(req,res){
     // res.send(dayWeek);
     //dayType = date();
     dayType=date.getDate();
-    res.render('list',{
-        dayWeek: dayType,
-        newListItem: item
+
+    // printing and rendering data from database
+    ItemModel.find({}).then(function(data){
+        if(data.length===0)
+        {
+            ItemModel.insertMany(docsArray);
+            res.redirect("/");
+        }
+        else{
+            res.render('list',{
+                dayWeek: dayType,
+                newListItem: data
+            });
+        }
     });
+    
 });
 
 app.post("/",function(req,res){
+    // let query=req.body.button;
+    // let itemName=req.body.newItem;
+    // if(query==="Work List")
+    // {
+    //     console.log("Inside work route...");
+    //     work.push(itemName);
+    //     res.redirect("/work");
+    // }
+    // else{
+    //     console.log("Inside home route...");
+    //     item.push(itemName);
+    //     res.redirect("/");
+    // }
+    
+
+    // using mongoose to post new items
+    const itemName = req.body.newItem;
     let query=req.body.button;
-    let itemName=req.body.newItem;
+
+    const itemDoc = new ItemModel({
+        name:itemName
+    });
+    itemDoc.save();
+    // data successfully added to database
+
     if(query==="Work List")
     {
         console.log("Inside work route...");
-        work.push(itemName);
         res.redirect("/work");
     }
     else{
         console.log("Inside home route...");
-        item.push(itemName);
         res.redirect("/");
     }
-    
-    
-    
+
 });
 
 // Adding Work route
@@ -53,4 +110,14 @@ app.get("/work",function(req,res){
 // Adding about route
 app.get("/about",function(req,res){
     res.render("about");
+});
+
+// Adding delete route
+app.post("/delete",function(req,res){
+    // console.log(req.body.checkbox);
+    const checkedID = req.body.checkbox;
+    ItemModel.findByIdAndRemove(checkedID).then(function(logs){
+        console.log(logs);
+        res.redirect("/");
+    });
 });
